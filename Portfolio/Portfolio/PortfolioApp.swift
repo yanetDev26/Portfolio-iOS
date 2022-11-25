ed73b07b55a78ab3825b361d3ebd281283def0cf
@yanet26 <yrodriguez@teco.com.ar>
2022-11-25 11:29:02 -0300

    create project

---
+++ Portfolio/Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Yanet Rodriguez on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
